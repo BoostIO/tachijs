498e19f2f26524582d95a5f370b960eb226f9659
Junyoung Choi <fluke8259@gmail.com>
2018-12-27 11:48:43 +0900

    Add req param decorators

--- src/handlerParam.ts
+++ src/handlerParam.ts
@@ -3,18 +3,18 @@ import express from 'express'
 
 const metaKey = MetaKey.handlerParam
 
-export type HandlerParamSelector = <T>(
+export type HandlerParamSelector<T> = (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => T
 
-export interface HandlerParamMeta {
+export interface HandlerParamMeta<T> {
   index: number
-  selector: HandlerParamSelector
+  selector: HandlerParamSelector<T>
 }
 
-export type HandlerParamListMeta = HandlerParamMeta[]
+export type HandlerParamListMeta = HandlerParamMeta<any>[]
 
 export function getHandlerParamListMeta(controller: any): HandlerParamListMeta {
   const metaList = Reflect.getMetadata(metaKey, controller)
@@ -29,7 +29,7 @@ export function setHandlerParamListMeta(
   Reflect.defineMetadata(metaKey, meta, controller)
 }
 
-export function handlerParam(selector: HandlerParamSelector) {
+export function handlerParam<T>(selector: HandlerParamSelector<T>) {
   return function controllerDecorator(
     target: any,
     propertyKey: string,
@@ -47,4 +47,40 @@ export function handlerParam(selector: HandlerParamSelector) {
 
     setHandlerParamListMeta(target.constructor, meta)
   }
+}
+
+export function reqParams(paramName?: string) {
+  const selector: HandlerParamSelector<any> =
+    paramName == null ? req => req.params : req => req.params[paramName]
+  return handlerParam(selector)
+}
+
+export function reqBody() {
+  return handlerParam(req => req.body)
+}
+
+export function reqQuery(paramName?: string) {
+  const selector: HandlerParamSelector<any> =
+    paramName == null ? req => req.query : req => req.query[paramName]
+  return handlerParam(selector)
+}
+
+export function reqHeaders(paramName?: string) {
+  const selector: HandlerParamSelector<any> =
+    paramName == null ? req => req.headers : req => req.headers[paramName]
+  return handlerParam(selector)
+}
+
+export function reqCookies(paramName?: string) {
+  const selector: HandlerParamSelector<any> =
+    paramName == null ? req => req.cookies : req => req.cookies[paramName]
+  return handlerParam(selector)
+}
+
+export function resRender() {
+  return handlerParam((req, res) => res.render)
+}
+
+export function nextFn() {
+  return handlerParam((req, res, next) => next)
 }
