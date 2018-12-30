9e1133c7d749a14a0878999875d380d7e119ce87
Junyoung Choi <fluke8259@gmail.com>
2018-12-30 19:23:14 +0900

    Configure test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/src/specs/setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
