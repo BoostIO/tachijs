498df79887080df42a72751f1e35cd74db0b592b
Junyoung Choi <fluke8259@gmail.com>
2018-12-31 15:15:36 +0900

    Add test for SendFileResult

---
+++ src/specs/results/SendFileResult.spec.ts
@@ -0,0 +1,102 @@
+import tachijs, { controller, httpGet, SendFileResult } from '../../index'
+import request from 'supertest'
+import path from 'path'
+
+describe('SendFileResult', () => {
+  it('is handled with res.sendFile', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendFileResult(path.join(__dirname, './files/readme.md'))
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 200,
+      text: '# Test document\n'
+    })
+  })
+
+  it('accepts options', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendFileResult('readme.md', {
+          root: path.join(__dirname, './files')
+        })
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 200,
+      text: '# Test document\n'
+    })
+  })
+
+  it('accepts callback', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendFileResult(
+          path.join(__dirname, './files/wrong.md'),
+          undefined,
+          (error, req, res) => {
+            res.send(error!.message)
+          }
+        )
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 200,
+      text: 'Not Found'
+    })
+  })
+
+  it('accepts status', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendFileResult(
+          path.join(__dirname, './files/readme.md'),
+          undefined,
+          undefined,
+          201
+        )
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 201,
+      text: '# Test document\n'
+    })
+  })
+})
