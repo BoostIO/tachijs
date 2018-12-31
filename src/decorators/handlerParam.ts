4dcb91275a695c9860e5cdc4da84eb8da60c72be
Junyoung Choi <fluke8259@gmail.com>
2019-01-01 01:05:48 +0900

    Add more decorators for cookie-parser module
    - setCookie
    - clearCookie
    - reqSignedCookies

--- src/decorators/handlerParam.ts
+++ src/decorators/handlerParam.ts
@@ -83,3 +83,27 @@ export function reqCookies(paramName?: string) {
     paramName == null ? req => req.cookies : req => req.cookies[paramName]
   return handlerParam(selector)
 }
+
+export interface CookieSetter {
+  (name: string, val: string, options: express.CookieOptions): express.Response
+  (name: string, val: any, options: express.CookieOptions): express.Response
+  (name: string, val: any): express.Response
+}
+
+export function setCookie() {
+  return handlerParam((req, res) => res.cookie.bind(res))
+}
+
+export type CookieClearer = (name: string, options?: any) => express.Response
+
+export function clearCookie() {
+  return handlerParam((req, res) => res.clearCookie.bind(res))
+}
+
+export function reqSignedCookies(paramName?: string) {
+  const selector: HandlerParamSelector<any> =
+    paramName == null
+      ? req => req.signedCookies
+      : req => req.signedCookies[paramName]
+  return handlerParam(selector)
+}
