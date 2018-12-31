60d350e0f9b63c765bb1e6e071ccdc8b97fa5141
Junyoung Choi <fluke8259@gmail.com>
2018-12-31 15:17:38 +0900

    Add test for SendResult

---
+++ src/specs/results/SendResult.spec.ts
@@ -0,0 +1,46 @@
+import tachijs, { controller, httpGet, SendResult } from '../../index'
+import request from 'supertest'
+
+describe('SendResult', () => {
+  it('is handled with res.send', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendResult('Hello')
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 200,
+      text: 'Hello'
+    })
+  })
+
+  it('accepts status', async () => {
+    // When
+    @controller('/')
+    class HomeController {
+      @httpGet('/')
+      index() {
+        return new SendResult('Hello', 201)
+      }
+    }
+
+    // Then
+    const app = tachijs({
+      controllers: [HomeController]
+    })
+    const response = await request(app).get('/')
+    expect(response).toMatchObject({
+      status: 201,
+      text: 'Hello'
+    })
+  })
+})
